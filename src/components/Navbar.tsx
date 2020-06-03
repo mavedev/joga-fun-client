ec18be9b67946e8b02d3d844f3ed6f999bd2f481
mavedev <usatchoff@gmail.com>
2020-06-03 20:00:14 +0300

    The Navbar component structure modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,38 +2,56 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {
   Navbar as BNavbar,
-  NavbarBrand,
-  Nav
+  NavbarBrand as BNavbarBrand,
+  Nav as BNav
 } from 'react-bootstrap';
 import 'styles/Navbar.scss';
 
 const Navbar = () => (
   <BNavbar className='navbar' expand='lg' bg='dark' sticky='top'>
-    <NavbarBrand>
+
+    {/* Bootstrap logo-like site name. */}
+    <BNavbarBrand>
       <Link className='navbar__link' to='/'>Joga.fun</Link>
-    </NavbarBrand>
+    </BNavbarBrand>
+
+    {/* The 'show/hide' button of the navbar. */}
     <BNavbar.Toggle aria-controls='basic-navbar-nav' />
-    <BNavbar.Collapse className='navbar__nav'>
-      {/* Right aligned nav items. */}
-      <Nav className='ml-auto navbar-nav'>
-        <ul className='nav navbar-nav navbar-right ml-auto'>
+
+    {/* Center-aligned nav items. */}
+    <BNavbar.Collapse className='navbar__navbar-collapse'>
+      {/* Bootstrap navbar link wrapper. */}
+      {/* Third-party classes: navbar-nav, ml-auto. */}
+      <BNav className='navbar__nav navbar-nav ml-auto'>
+        {/* Inner list of links. */}
+        {/* Third-party classes: nav, navbar-nav, ml-auto. */}
+        <ul className='nav navbar-nav ml-auto'>
+          {/* The Facebook link. */}
+          {/* Third-party classes: nav-item. */}
           <li className='nav-item'>
-            <Nav.Link href='/' className='nav-link'>
+            <BNav.Link href='/'>
+              {/* Third-party classes: fa, fa-*. */}
               <i className='fa fa-facebook navbar__link' />
-            </Nav.Link>
+            </BNav.Link>
           </li>
+          {/* The Twitter link. */}
+          {/* Third-party classes: nav-item. */}
           <li className='nav-item'>
-            <Nav.Link href='/' className='nav-link'>
+            <BNav.Link href='/'>
+              {/* Third-party classes: fa, fa-*. */}
               <i className='fa fa-twitter navbar__link' />
-            </Nav.Link>
+            </BNav.Link>
           </li>
+          {/* The VK link. */}
+          {/* Third-party classes: nav-item. */}
           <li className='nav-item'>
-            <Nav.Link href='/' className='nav-link'>
+            <BNav.Link href='/'>
+              {/* Third-party classes: fa, fa-*. */}
               <i className='fa fa-vk navbar__link' />
-            </Nav.Link>
+            </BNav.Link>
           </li>
         </ul>
-      </Nav>
+      </BNav>
     </BNavbar.Collapse>
   </BNavbar>
 );
