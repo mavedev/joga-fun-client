e235997bbb92c6974351cd9d14f573d41c2c4042
mavedev <usatchoff@gmail.com>
2020-06-04 15:46:18 +0300

    The Footer elements created

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import 'styles/Footer.scss';
 
 const Footer = () => (
   <div className='footer-wrapper'>
-    <div className='footer-wrapper__main' />
-    <div className='footer-wrapper__copiright' />
+    <div className='footer-wrapper__main'>
+      <p>MainF</p>
+    </div>
+    <div className='footer-wrapper__copiright-wrapper'>
+      <span className='footer-wrapper__copiright'>
+        &copy; 2020 Copiright:
+      </span>
+      <span className='footer-wrapper__text-important'>
+        JogaFun
+      </span>
+    </div>
   </div>
 );
 
