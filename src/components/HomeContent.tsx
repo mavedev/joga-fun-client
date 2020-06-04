fe75968d97fdf8b3c208405059638f2f4612dcb4
mavedev <usatchoff@gmail.com>
2020-06-04 19:39:59 +0300

    The Calendar styles included

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const HomeContent = () => (
-  <Calendar />
+  <Calendar locale='ru-ru' />
 );
 
 export default HomeContent;
