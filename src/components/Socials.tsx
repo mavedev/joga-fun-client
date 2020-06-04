2ecb1b27ee42d762d3c2d5c0be4467ee50a57846
mavedev <usatchoff@gmail.com>
2020-06-04 19:51:01 +0300

    Social buttons structure provided

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import {
+  Col as BColumn,
+  Row as BRow
+} from 'react-bootstrap';
 
 const Socials = () => (
-  <div />
+  <div>
+    <BColumn>
+      <h3 className='my-4 align-center'>Мы в соцсетях:</h3>
+      <BRow className='justify-content-center align-items-center'>
+        <a
+          href='http://scripteden.com/download/eden-ui/'
+          className='btn-social btn-facebook'
+        >
+          <i className='fa fa-facebook' />
+        </a>
+        <a
+          href='http://scripteden.com/download/eden-ui/'
+          className='btn-social btn-twitter'
+        >
+          <i className='fa fa-twitter' />
+        </a>
+        <a
+          href='http://scripteden.com/download/eden-ui/'
+          className='btn-social btn-vk'
+        >
+          <i className='fa fa-vk' />
+        </a>
+      </BRow>
+    </BColumn>
+  </div>
 );
 
 export default Socials;
