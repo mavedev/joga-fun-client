907a2429070d30486ad2ea45370e141ff7bce108
mavedev <usatchoff@gmail.com>
2020-06-04 22:56:00 +0300

    Social buttons styles modified

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -4,27 +4,38 @@ import {
   Row as BRow
 } from 'react-bootstrap';
 
+import 'styles/Socials.scss';
+
 const Socials = () => (
   <div>
-    <BColumn>
-      <h3 className='my-4 align-center'>Мы в соцсетях:</h3>
+    <BColumn className='socials'>
+      {/* Third-party classes: my-4. */}
+      <h3 className='my-4 socials__title'>Мы в соцсетях:</h3>
+      {/* The social buttons themselves. */}
+      {/* Third-party classes: justify-content-center, align-items-center. */}
       <BRow className='justify-content-center align-items-center'>
+        {/* Facebook. */}
         <a
-          href='http://scripteden.com/download/eden-ui/'
-          className='btn-social btn-facebook'
+          href='https://facebook.com'
+          className='socials__button socials__button--facebook'
         >
+          {/* Third-party style (Font Awesome). */}
           <i className='fa fa-facebook' />
         </a>
+        {/* Twitter. */}
         <a
-          href='http://scripteden.com/download/eden-ui/'
-          className='btn-social btn-twitter'
+          href='https://twitter.com'
+          className='socials__button socials__button--twitter'
         >
+          {/* Third-party style (Font Awesome). */}
           <i className='fa fa-twitter' />
         </a>
+        {/* VK. */}
         <a
-          href='http://scripteden.com/download/eden-ui/'
-          className='btn-social btn-vk'
+          href='https://vk.com'
+          className='socials__button socials__button--vk'
         >
+          {/* Third-party style (Font Awesome). */}
           <i className='fa fa-vk' />
         </a>
       </BRow>
