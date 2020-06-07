eaca7e2d57271cb61d9cea52111952b46370838c
mavedev <usatchoff@gmail.com>
2020-06-07 19:59:45 +0300

    Content bottom padding provided

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -15,31 +15,33 @@ const category = 'Категория 1';
 
 const HomeContent = () => (
   <BContainer className='content'>
-    <BRow>
-      {/* Posts column. 8/12 of the page width. */}
-      <BCol md='8' className='content__column'>
-        {/* The publications block title. */}
-        {/* Third-party classes: my-4. */}
-        <h1 className='content__title my-4'>
-          Публикации
-          <small>
-            {' '}
-            {category}
-            {' '}
-          </small>
-        </h1>
-        <div>
-          <p>Текст Публикации</p>
-        </div>
-      </BCol>
+    <div className='row-wrapper'>
+      <BRow>
+        {/* Posts column. 8/12 of the page width. */}
+        <BCol md='8' className='content__column'>
+          {/* The publications block title. */}
+          {/* Third-party classes: my-4. */}
+          <h1 className='content__title my-4'>
+            Публикации
+            <small>
+              {' '}
+              {category}
+              {' '}
+            </small>
+          </h1>
+          <div>
+            <p>Текст Публикации</p>
+          </div>
+        </BCol>
 
-      {/* Sidebar column. 4/12 of the page width. */}
-      <BCol md='4' className='content__column'>
-        <Categories />
-        <Calendar locale='ru-ru' />
-        <Socials />
-      </BCol>
-    </BRow>
+        {/* Sidebar column. 4/12 of the page width. */}
+        <BCol md='4' className='content__column'>
+          <Categories />
+          <Calendar locale='ru-ru' />
+          <Socials />
+        </BCol>
+      </BRow>
+    </div>
   </BContainer>
 );
 
