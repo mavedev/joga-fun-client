bb192279d96e1210e996ba4a690e46835f9b781f
mavedev <usatchoff@gmail.com>
2020-06-07 20:13:29 +0300

    The CalendarWrapper structure provided

--- src/components/CalendarWrapper.tsx
+++ src/components/CalendarWrapper.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Calendar from 'react-calendar';
+import {
+  Card as BCard
+} from 'react-bootstrap';
 
 const CalendarWrapper = () => (
-  <div />
+  <div className='top-level-wrapper'>
+    <BCard className='my-4'>
+      <Calendar />
+    </BCard>
+  </div>
 );
 
 export default CalendarWrapper;
