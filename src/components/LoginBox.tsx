050e241ebbdd1de66455e68e7696f580ead8b65a
mavedev <usatchoff@gmail.com>
2020-06-07 23:53:40 +0300

    LoginBox basic structure provided

--- src/components/LoginBox.tsx
+++ src/components/LoginBox.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const LoginBox = () => (
-  <div />
+  <div className='filler'>
+    <div className='container col-lg-4'>
+      <div className='jumbotron'>
+        <h3 className='text-center'>Вход администратора</h3>
+        <form>
+          <div className='form-group'>
+            <input type='text' className='form-control' placeholder='Логин' />
+          </div>
+          <div className='form-group'>
+            <input type='password' className='form-control' placeholder='Пароль' />
+          </div>
+          <button type='submit' className='btn btn-primary form-control'>Вход</button>
+        </form>
+      </div>
+    </div>
+  </div>
 );
 
 export default LoginBox;
