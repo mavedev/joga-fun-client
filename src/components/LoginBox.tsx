839de533f3408e0978bd8fa96da529d57a9e4ee2
mavedev <usatchoff@gmail.com>
2020-06-09 19:28:58 +0300

    The LoginBox styles changed to use SCSS modules

--- src/components/LoginBox.tsx
+++ src/components/LoginBox.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import cx from 'classnames';
 
-import 'styles/LoginBox.scss';
+import styles from 'styles/LoginBox.module.scss';
 
 const LoginBox = () => (
-  <div className='filler'>
-    <div className='container col-lg-4'>
-      <div className='jumbotron'>
+  <div>
+    {/* Additional wrapper for paddings. */}
+    <div className={styles['box-wrapper']}>
+      {/* The main container. */}
+      {/* Third-party classes: container, jumbotron, col-lg-4. */}
+      <div className={cx('container', 'jumbotron', 'col-lg-4')}>
+        {/* The title. */}
         <h3 className='text-center'>Вход администратора</h3>
+        {/* The login form. */}
         <form>
+          {/* Third=party classes: form-group. */}
           <div className='form-group'>
+            {/* Third=party classes: form-control. */}
             <input type='text' className='form-control' placeholder='Логин' />
           </div>
+          {/* Third=party classes: form-group. */}
           <div className='form-group'>
+            {/* Third=party classes: form-control. */}
             <input type='password' className='form-control' placeholder='Пароль' />
           </div>
+          {/* The submit button. */}
+          {/* Third=party classes: btn, btn-primary, form-control. */}
           <button type='submit' className='btn btn-primary form-control'>Вход</button>
         </form>
       </div>
