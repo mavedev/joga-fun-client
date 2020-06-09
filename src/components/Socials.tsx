0d655c43f866322aed846e934e06339fc76211f4
mavedev <usatchoff@gmail.com>
2020-06-09 19:41:56 +0300

    The Socials styles changed to use SCSS modules

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,42 +1,52 @@
 import React from 'react';
+import cx from 'classnames';
 import {
   Col as BColumn,
   Row as BRow
 } from 'react-bootstrap';
 
-import 'styles/Socials.scss';
+import styles from 'styles/Socials.module.scss';
 
 const Socials = () => (
   <div>
-    <BColumn className='socials'>
+    <BColumn>
       {/* Third-party classes: my-4. */}
-      <h3 className='my-4 socials__title'>Мы в соцсетях:</h3>
+      <h3 className={cx('my-4', styles['socials__title'])}>Мы в соцсетях:</h3>
       {/* The social buttons themselves. */}
       {/* Third-party classes: justify-content-center, align-items-center. */}
-      <BRow className='justify-content-center align-items-center'>
+      <BRow className={cx('justify-content-center', 'align-items-center')}>
         {/* Facebook. */}
         <a
           href='https://facebook.com'
-          className='socials__button socials__button--facebook'
+          className={cx(
+            styles['socials__button'],
+            styles['socials__button--facebook']
+          )}
         >
           {/* Third-party style (Font Awesome). */}
-          <i className='fa fa-facebook' />
+          <i className={cx('fa', 'fa-facebook')} />
         </a>
         {/* Twitter. */}
         <a
           href='https://twitter.com'
-          className='socials__button socials__button--twitter'
+          className={cx(
+            styles['socials__button'],
+            styles['socials__button--twitter']
+          )}
         >
           {/* Third-party style (Font Awesome). */}
-          <i className='fa fa-twitter' />
+          <i className={cx('fa', 'fa-twitter')} />
         </a>
         {/* VK. */}
         <a
           href='https://vk.com'
-          className='socials__button socials__button--vk'
+          className={cx(
+            styles['socials__button'],
+            styles['socials__button--vk']
+          )}
         >
           {/* Third-party style (Font Awesome). */}
-          <i className='fa fa-vk' />
+          <i className={cx('fa', 'fa-vk')} />
         </a>
       </BRow>
     </BColumn>
