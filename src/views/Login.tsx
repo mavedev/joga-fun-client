c5a4fe24d176b094ed6ccedc00b29d80705714a3
mavedev <usatchoff@gmail.com>
2020-06-09 19:33:46 +0300

    Footer removed from The Login view

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from 'components/Navbar';
-import Footer from 'components/Footer';
 import LoginBox from 'components/LoginBox';
 
 const Home = () => (
@@ -8,9 +7,6 @@ const Home = () => (
     <div className='home-area home-area--gradient-hot-cold'>
       <Navbar />
       <LoginBox />
-    </div>
-    <div className='footer-wrapper'>
-      <Footer />
     </div>
   </div>
 );
