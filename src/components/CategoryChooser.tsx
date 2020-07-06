295fbec764acb942cab14b0ac16a7a713a2d7387
Cirill Usatchoff <usatchoff@gmail.com>
2020-07-06 19:31:15 +0300

    Categories choose provided

--- src/components/CategoryChooser.tsx
+++ src/components/CategoryChooser.tsx
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Dropdown } from 'react-bootstrap';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
 
 import cn from 'styles/CategoryChooser.module.scss';
 
-const CategoryChooser = () => (
-  <div className={cn['MainWrapper']}>
-    <div className={cn['CategoryTextHintWrapper']}>
-      <p className={cn['CategoryTextHint']}>Категория:</p>
+const CategoryChooser = () => {
+  const [title, setTitle] = useState('');
+  const categories = ['A', 'B', 'C'];
+  return (
+    <div className={cn['MainWrapper']}>
+      <div className={cn['CategoryTextHintWrapper']}>
+        <p className={cn['CategoryTextHint']}>Категория:</p>
+      </div>
+
+      <DropdownButton
+        variant='light'
+        alignRight
+        title={title}
+        id='dropdown-menu-align-right'
+        onSelect={(eventKey: number) => { setTitle(categories[+eventKey]); }}
+      >
+        {
+          categories.map((value: string, index: number) => (
+            <Dropdown.Item key={value} eventKey={`${index}`}>
+              {value}
+            </Dropdown.Item>
+          ))
+        }
+      </DropdownButton>
     </div>
-
-    <Dropdown>
-      <Dropdown.Toggle variant='light' id='dropdown-basic'>
-        Dropdown Button
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item>Action</Dropdown.Item>
-        <Dropdown.Item>Another action</Dropdown.Item>
-        <Dropdown.Item>Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  </div>
-);
+  );
+};
 
 export default CategoryChooser;
