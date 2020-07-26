79e4a3b5ab086165a2edd85c4891407b6c9693a8
Cirill Usatchoff <usatchoff@gmail.com>
2020-07-26 17:48:54 +0300

    The store connected to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
+
+import { store } from 'store';
+
 import HomeView from 'views/HomeView';
 import LoginView from 'views/LoginView';
 import NewPostView from 'views/NewPostView';
+
 import './App.scss';
 
-const App = () => (
-  <Router>
-    <div className='App'>
-      <Switch>
-        <Route path='/' exact component={HomeView} />
-        <Route path='/login' exact component={LoginView} />
-        <Route path='/manage' exact component={NewPostView} />
-      </Switch>
-    </div>
-  </Router>
+const App: React.FC = () => (
+  <Provider store={store}>
+    <Router>
+      <div className='App'>
+        <Switch>
+          <Route path='/' exact component={HomeView} />
+          <Route path='/login' exact component={LoginView} />
+          <Route path='/manage' exact component={NewPostView} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
 );
 
 export default App;
