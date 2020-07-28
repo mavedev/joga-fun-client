3a6aeb3c020bddc51dc0176677701534b4b08efa
Cirill Usatchoff <usatchoff@gmail.com>
2020-07-28 16:04:28 +0300

    Login components structure updated

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 import Navbar from 'components/Navbar';
-import LoginBox from 'components/LoginBox';
+import LoginBox from 'components/LoginView/LoginBox';
 
 import styles from 'styles/View.module.scss';
 
