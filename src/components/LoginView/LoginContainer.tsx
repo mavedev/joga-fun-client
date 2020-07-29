7cd650ec18a74ff89e30177b7c57215e1ac13852
Cirill Usatchoff <usatchoff@gmail.com>
2020-07-29 16:27:25 +0300

    Login prop provided for the LoginContainer

--- src/components/LoginView/LoginContainer.tsx
+++ src/components/LoginView/LoginContainer.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { createLoginThunk } from 'store';
+import { AppState, createLoginThunk } from 'store';
 import LoginBox from './LoginBox';
+
+type MapStateProps = {
+  isLoggedIn: boolean;
+}
 
 type MapDispatchProps = {
   doLogin: (username: string, password: string) => void;
 }
+
+const mapStateToProps = (state: AppState): MapStateProps => ({
+  isLoggedIn: state.auth.isLoggedIn
+});
 
 const mapDispatchToProps: MapDispatchProps = {
   doLogin: createLoginThunk
 };
 
 type OwnProps = {}
-type AllProps = MapDispatchProps & OwnProps;
+type AllProps = MapStateProps & MapDispatchProps & OwnProps;
 
-const LoginContainer: React.FC<AllProps> = ({ doLogin }: AllProps) => (
+const LoginContainer: React.FC<AllProps> = ({ isLoggedIn, doLogin }: AllProps) => (
   <>
     <LoginBox doLoginCallback={doLogin} />
   </>
 );
 
-export default connect(null, mapDispatchToProps)(LoginContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginContainer);
