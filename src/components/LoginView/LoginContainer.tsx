5650a7186dd54398b2dc44f830ad4e9b5754bb5e
Cirill Usatchoff <usatchoff@gmail.com>
2020-07-30 18:10:12 +0300

    The LoginContainer composing typing fixed

--- src/components/LoginView/LoginContainer.tsx
+++ src/components/LoginView/LoginContainer.tsx
@@ -6,32 +6,23 @@ import { createLoginThunk, AppState } from 'store';
 import { withAuthRedirect } from 'hoc';
 import LoginBox from './LoginBox';
 
-type MapStateProps = {
-  isLoggedIn: boolean;
-}
-
 type MapDispatchProps = {
   doLogin: (username: string, password: string) => void;
 }
-
-const mapStateToProps = (state: AppState): MapStateProps => ({
-  isLoggedIn: state.auth.isLoggedIn
-});
 
 const mapDispatchToProps: MapDispatchProps = {
   doLogin: createLoginThunk
 };
 
+type MapStateProps = {}
 type OwnProps = {}
 type AllProps = MapStateProps & MapDispatchProps & OwnProps;
 
 const LoginContainer = ({ doLogin }: AllProps) => (
-  <>
-    <LoginBox doLoginCallback={doLogin} />
-  </>
+  <LoginBox doLoginCallback={doLogin} />
 );
 
-export default compose(
+export default compose<React.ComponentType<OwnProps>>(
   withAuthRedirect,
-  connect(mapStateToProps, mapDispatchToProps)
+  connect(null, mapDispatchToProps)
 )(LoginContainer);
