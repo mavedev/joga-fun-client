7bc0998cb293c59902ecc3bbb554b2a7fbe62e49
Cirill Usatchoff <usatchoff@gmail.com>
2020-07-30 19:29:16 +0300

    Set JWT action creator provided and applied

--- src/store/Auth/actions.ts
+++ src/store/Auth/actions.ts
@@ -2,18 +2,24 @@ import { Dispatch } from 'redux';
 
 import API from 'api';
 
-import { DO_LOGIN, LoginActionType } from './types';
+import { DO_LOGIN, SET_JWT, LoginActionType } from './types';
 import { AppActionType } from '../types';
 
 export const createSetLoggedIn = (): LoginActionType => ({
   type: DO_LOGIN
 });
 
+export const createSetJWT = (jwt: string): LoginActionType => ({
+  type: SET_JWT,
+  payload: jwt
+});
+
 export const createLoginThunk = (
   username: string,
   password: string
 ) => (dispatch: Dispatch<AppActionType>) => {
-  API.login(username, password).then(() => {
+  API.login(username, password).then((response) => {
     dispatch(createSetLoggedIn());
+    dispatch(createSetJWT(response.data['token']));
   });
 };
