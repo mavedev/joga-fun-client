8a48d30491a777c6ed7982ab90c7257c7a90778a
Cirill Usatchoff <usatchoff@gmail.com>
2020-07-31 17:07:35 +0300

    The LoginContainer default export provided

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 import Navbar from 'components/Navbar';
-import LoginContainer from 'components/LoginView/LoginContainer';
+import LoginContainer from 'components/LoginView';
 
 import styles from 'styles/View.module.scss';
 
