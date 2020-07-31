df3d4f7f849426753bb6ee1c026020acb9d4ab95
Cirill Usatchoff <usatchoff@gmail.com>
2020-07-31 15:47:19 +0300

    The Preloader applied

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,7 +7,7 @@ import {
 } from 'react-router-dom';
 
 import { store } from 'store';
-
+import Preloader from 'components/Preloading';
 import HomeView from 'views/HomeView';
 import LoginView from 'views/LoginView';
 import NewPostView from 'views/NewPostView';
@@ -16,15 +16,17 @@ import 'styles/App/App.scss';
 
 const App: React.FC = () => (
   <Provider store={store}>
-    <Router>
-      <div className='App'>
-        <Switch>
-          <Route path='/' exact component={HomeView} />
-          <Route path='/login' exact component={LoginView} />
-          <Route path='/manage' exact component={NewPostView} />
-        </Switch>
-      </div>
-    </Router>
+    <Preloader>
+      <Router>
+        <div className='App'>
+          <Switch>
+            <Route path='/' exact component={HomeView} />
+            <Route path='/login' exact component={LoginView} />
+            <Route path='/manage' exact component={NewPostView} />
+          </Switch>
+        </div>
+      </Router>
+    </Preloader>
   </Provider>
 );
 
