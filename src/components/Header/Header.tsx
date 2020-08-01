6fcd87ed9d7e434e972ed77973873266e25f1c0f
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-01 21:33:51 +0300

    app-translator removed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import getText from 'translations';
 import styles from 'styles/Header/Header.module.scss';
 
 const Header = () => (
   <div className={styles['header-wrapper']}>
     <div className={styles['header-wrapper__overlay']}>
       <h1 className={styles['header-wrapper__title']}>
-        {getText('headerTitle')}
+        Мир йоги
       </h1>
       <h3 className={styles['header-wrapper__text']}>[Описание]</h3>
       <p className={styles['header-wrapper__text']}>[Развёрнутое описание]</p>
