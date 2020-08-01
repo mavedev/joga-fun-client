c3b5830242833f4c901bed75e6374dbd52e891c6
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-02 00:13:07 +0300

    The Header styles naming updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import styles from 'styles/Header/Header.module.scss';
+import cn from 'styles/Header/Header.module.scss';
 
+/* Main header component. */
 const Header = () => {
   const { t } = useTranslation();
 
   return (
-    <div className={styles['header-wrapper']}>
-      <div className={styles['header-wrapper__overlay']}>
-        <h1 className={styles['header-wrapper__title']}>
+    <div className={cn['Header-wrapper']}>
+      <div className={cn['Header-wrapper__overlay']}>
+        <h1 className={cn['Header-wrapper__title']}>
           {t('Title.1')}
         </h1>
-        <h3 className={styles['header-wrapper__text']}>
+        <h3 className={cn['Header-wrapper__text']}>
           {t('Description.1')}
         </h3>
-        <p className={styles['header-wrapper__text']}>
+        <p className={cn['Header-wrapper__text']}>
           {t('Description.2')}
         </p>
       </div>
