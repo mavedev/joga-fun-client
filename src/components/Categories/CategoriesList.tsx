d07bf6773f06c7c39d4ffb192a78785d14c70cb5
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-02 18:35:24 +0300

    The CategoriesList component translated

--- src/components/Categories/CategoriesList.tsx
+++ src/components/Categories/CategoriesList.tsx
@@ -3,6 +3,7 @@ import {
   Card as BCard,
   Row as BRow
 } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import cn from 'styles/Categories/CategoriesList.module.scss';
@@ -12,23 +13,27 @@ import CategoryItem from './CategoryItem';
 /* Normal component's props that are to be passed. */
 type OwnProps = { categories: string[]; };
 
-const CategoriesList: React.FC<OwnProps> = ({ categories }: OwnProps) => (
-  <div>
-    <BCard className='my-4'>
-      <h5 className='card-header'>Категории</h5>
-      <BCard.Body className={cn['ListWrapper--Centered']}>
-        <BRow>
-          <div className='col-lg-12'>
-            <ul className='list-unstyled mb-0'>
-              {categories.map((value: string) => (
-                <CategoryItem key={value} name={value} link='/' />
-              ))}
-            </ul>
-          </div>
-        </BRow>
-      </BCard.Body>
-    </BCard>
-  </div>
-);
+const CategoriesList: React.FC<OwnProps> = ({ categories }: OwnProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <BCard className='my-4'>
+        <h5 className='card-header'>{t('Categories.1')}</h5>
+        <BCard.Body className={cn['ListWrapper--Centered']}>
+          <BRow>
+            <div className='col-lg-12'>
+              <ul className='list-unstyled mb-0'>
+                {categories.map((value: string) => (
+                  <CategoryItem key={value} name={value} link='/' />
+                ))}
+              </ul>
+            </div>
+          </BRow>
+        </BCard.Body>
+      </BCard>
+    </div>
+  );
+};
 
 export default CategoriesList;
