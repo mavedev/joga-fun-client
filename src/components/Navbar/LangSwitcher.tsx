29f42558c148e418b260711427b9c5a670ca352f
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-02 17:19:36 +0300

    The LangSwitcher styles updated

--- src/components/Navbar/LangSwitcher.tsx
+++ src/components/Navbar/LangSwitcher.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Button as BButton } from 'react-bootstrap';
 
+import 'styles/overrides/bootstrap.scss';
+import cn from 'styles/Navbar/LangSwitcher.module.scss';
+
 /* Normal props of the component. */
 type OwnProps = { langs: string[]; };
 
 /* Panel with buttons to switch interface language. */
 const LangSwitcher: React.FC<OwnProps> = ({ langs }: OwnProps) => (
-  <div>
-    {langs.map((value) => <BButton>{value.toUpperCase()}</BButton>)}
+  <div className={cn['MainWrapper']}>
+    {langs.map((value) => (
+      <div key={value} className={cn['BButtonWrapper']}>
+        <BButton className='BButton' variant='light'>
+          {value.toUpperCase()}
+        </BButton>
+      </div>
+    ))}
   </div>
 );
 
