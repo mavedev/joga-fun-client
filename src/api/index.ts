eedc52a278a27241e44f675ba0c337904828b905
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-04 20:17:11 +0300

    Posts quantity retrieving provided in the API

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,19 +3,23 @@ import axios, { AxiosResponse } from 'axios';
 type APIResponse = Promise<AxiosResponse<any>>;
 
 class API {
-  private static adminAPI = axios.create({
+  private static instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL
   });
 
-  public static getCategories = (): APIResponse => API.adminAPI.get(
+  public static getCategories = (): APIResponse => API.instance.get(
     'categories/50'
   );
 
   public static login = (
     username: string,
     password: string
-  ): APIResponse => API.adminAPI
+  ): APIResponse => API.instance
     .post('login', {}, { auth: { username, password } })
+
+  public static getPostsQuantity = (): APIResponse => API.instance.get(
+    'posts/quantity', {}
+  )
 }
 
 export default API;
