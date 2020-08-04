dc5860ea6058506798958f2d335f241bac8ce5da
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-04 21:21:29 +0300

    The HomeContainer connected to the store

--- src/components/HomeView/HomeContainer.tsx
+++ src/components/HomeView/HomeContainer.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const HomeContainer: React.FC<{}> = () => (
+import { AppState, createFilterSetCurrentCategory } from 'store';
+
+/* Store state props type. */
+type MapStateProps = { postsQuantity: number; };
+/* Store dispatch props type. */
+type MapDispatchProps = {
+  setCurrentFilteredCategory: (category: string) => void;
+};
+/* Normal component's props that are to be passed. */
+type OwnProps = {};
+/* All props type. */
+type AllProps = MapStateProps & MapDispatchProps & OwnProps;
+
+const mapStatetoProps = (state: AppState): MapStateProps => ({
+  postsQuantity: state.posts.postsQuantity
+});
+
+const mapDispatchToProps: MapDispatchProps = {
+  setCurrentFilteredCategory: createFilterSetCurrentCategory
+};
+
+const HomeContainer: React.FC<AllProps> = ({
+  postsQuantity, setCurrentFilteredCategory
+}: AllProps) => (
   <></>
 );
 
-export default HomeContainer;
+export default connect(
+  mapStatetoProps,
+  mapDispatchToProps
+)(HomeContainer);
