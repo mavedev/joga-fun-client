c689dd5bb781115f9ad7c42f52a2cfd564911828
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-05 20:05:32 +0300

    The PostsContainer parameter changed

--- src/components/HomeView/Posts/PostsContainer.tsx
+++ src/components/HomeView/Posts/PostsContainer.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const PostsContainer: React.FC<{}> = () => (
-  <div />
+import { Post } from 'store';
+
+/* Store state props type. */
+type MapStateProps = { currentPostsChunk: Post[]; };
+/* Store dispatch props type. */
+type MapDispatchProps = {};
+/* Normal component's props that are to be passed. */
+type OwnProps = {};
+/* All props type. */
+type AllProps = MapStateProps & MapDispatchProps & OwnProps;
+
+const PostsContainer: React.FC<AllProps> = ({ currentPostsChunk }: AllProps) => (
+  
 );
 
 export default PostsContainer;
