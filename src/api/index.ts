aacdcedd4acbee53d622b79b74c29ca41930af6f
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-06 19:33:51 +0300

    The API rewritten to use more readable format

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,31 +2,31 @@ import axios, { AxiosResponse } from 'axios';
 
 type APIResponse = Promise<AxiosResponse<any>>;
 
-class API {
+/* The main API to communicate with the server.  */
+export default class API {
   private static instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL
   });
 
-  public static getCategories = (): APIResponse => API.instance.get(
-    'categories'
-  );
+  public static getCategories = (): APIResponse => {
+    return API.instance.get('categories');
+  }
 
   public static login = (
     username: string,
     password: string
-  ): APIResponse => API.instance
-    .post('login', {}, { auth: { username, password } })
+  ): APIResponse => {
+    return API.instance.post('login', {}, { auth: { username, password } });
+  }
 
   public static getPostsChunk = (
     filteredCategory: string | null,
     chunk: number
-  ): APIResponse => API.instance.get(
-    `posts/${filteredCategory || 'all'}/${chunk}`, {}
-  );
+  ): APIResponse => {
+    return API.instance.get(`posts/${filteredCategory || 'all'}/${chunk}`, {});
+  }
 
-  public static getPostsQuantity = (): APIResponse => API.instance.get(
-    'posts/quantity', {}
-  );
+  public static getPostsQuantity = (): APIResponse => {
+    return API.instance.get('posts/quantity', {});
+  }
 }
-
-export default API;
