8061784dd0645d393095fd5e5385e1601f0ce480
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-06 23:10:17 +0300

    Locales and links provided for the Socials section

--- src/components/HomeView/Sidebar/Socials.tsx
+++ src/components/HomeView/Sidebar/Socials.tsx
@@ -1,56 +1,49 @@
 import React from 'react';
 import cx from 'classnames';
-import {
-  Col as BColumn,
-  Row as BRow
-} from 'react-bootstrap';
+import { Col as BColumn, Row as BRow } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
-import styles from 'styles/HomeView/Socials.module.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import cn from 'styles/HomeView/Socials.module.scss';
 
-const Socials: React.FC<{}> = () => (
-  <div>
-    <BColumn>
-      {/* Third-party classes: my-4. */}
-      <h3 className={cx('my-4', styles['socials__title'])}>Мы в соцсетях:</h3>
-      {/* The social buttons themselves. */}
-      {/* Third-party classes: justify-content-center, align-items-center. */}
-      <BRow className={cx('justify-content-center', 'align-items-center')}>
-        {/* Facebook. */}
-        <a
-          href='https://facebook.com'
-          className={cx(
-            styles['socials__button'],
-            styles['socials__button--facebook']
-          )}
-        >
-          {/* Third-party style (Font Awesome). */}
-          <i className={cx('fa', 'fa-facebook')} />
-        </a>
-        {/* Twitter. */}
-        <a
-          href='https://twitter.com'
-          className={cx(
-            styles['socials__button'],
-            styles['socials__button--twitter']
-          )}
-        >
-          {/* Third-party style (Font Awesome). */}
-          <i className={cx('fa', 'fa-twitter')} />
-        </a>
-        {/* VK. */}
-        <a
-          href='https://vk.com'
-          className={cx(
-            styles['socials__button'],
-            styles['socials__button--vk']
-          )}
-        >
-          {/* Third-party style (Font Awesome). */}
-          <i className={cx('fa', 'fa-vk')} />
-        </a>
-      </BRow>
-    </BColumn>
-  </div>
-);
+const Socials: React.FC<{}> = () => {
+  const { t: translator } = useTranslation();
+
+  return (
+    <div>
+      <BColumn>
+        <h3 className={cx('my-4', cn['socials__title'])}>
+          {translator('OurSocials')}
+        </h3>
+        {/* The social buttons themselves. */}
+        <BRow className={cx('justify-content-center', 'align-items-center')}>
+          {/* Facebook. */}
+          <a
+            href={process.env.REACT_APP_LINK_FB}
+            className={cx(cn['socials__button'], cn['socials__button--facebook'])}
+          >
+            <i className={cx('fa', 'fa-facebook')} />
+          </a>
+
+          {/* Twitter. */}
+          <a
+            href={process.env.REACT_APP_LINK_TW}
+            className={cx(cn['socials__button'], cn['socials__button--twitter'])}
+          >
+            <i className={cx('fa', 'fa-twitter')} />
+          </a>
+
+          {/* VK. */}
+          <a
+            href={process.env.REACT_APP_LINK_VK}
+            className={cx(cn['socials__button'], cn['socials__button--vk'])}
+          >
+            <i className={cx('fa', 'fa-vk')} />
+          </a>
+        </BRow>
+      </BColumn>
+    </div>
+  );
+};
 
 export default Socials;
