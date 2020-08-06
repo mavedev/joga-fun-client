1b47da383bf25f2bdfe086af20605e7ade65a1a5
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-06 21:32:08 +0300

    Post preview translation provided

--- src/components/HomeView/Posts/Post.tsx
+++ src/components/HomeView/Posts/Post.tsx
@@ -1,47 +1,44 @@
 import React from 'react';
-import {
-  Card as BCard
-} from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { Card as BCard } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 import { PostDTO } from 'dal';
 import cx from 'classnames';
-import styles from 'styles/HomeView/Posts/Post.module.scss';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 /* Normal component's props that are to be passed. */
 type OwnProps = { data: PostDTO; };
 
-const Post: React.FC<OwnProps> = ({ data }: OwnProps) => (
-  <div className='MainWrapper'>
-    <BCard>
-      <img
-        className='card-img-top'
-        src={data.imageURL}
-        alt=''
-        crossOrigin='anonymous'
-      />
-      <div className='card-body'>
-        <h2 className='card-title'>{data.title}</h2>
-        <p
-          className='card-text'
-        >
-          {`${data.body.slice(0, 100)} ...`}
-        </p>
-        <a
-          href='/'
-          className={cx(
-            'btn',
-            'btn-primary',
-            styles['disabled-link']
-          )}
-        >
-          Читать полностью &rarr;
-        </a>
-      </div>
-      <div className='card-footer text-muted'>
-        <p>{data.created}</p>
-      </div>
-    </BCard>
-  </div>
-);
+const Post: React.FC<OwnProps> = ({ data }: OwnProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className='MainWrapper'>
+      <BCard>
+        <img
+          className='card-img-top'
+          src={data.imageURL}
+          alt=''
+          crossOrigin='anonymous'
+        />
+        <div className='card-body'>
+          <h2 className='card-title'>{data.title}</h2>
+          <p className='card-text'>
+            {`${data.body.slice(0, 100)} ...`}
+          </p>
+          <NavLink to='/login' className={cx('btn', 'btn-primary')}>
+            {`${t('ReadFull')} `}
+            &rarr;
+          </NavLink>
+        </div>
+        <div className='card-footer text-muted'>
+          <p>{data.created}</p>
+        </div>
+      </BCard>
+    </div>
+  );
+};
 
 export default Post;
