8a406cd4a2632c3837673dd63d83b625578a9634
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-07 19:05:08 +0300

    DTO type check incorrect behavior fixed

--- src/dal/types.ts
+++ src/dal/types.ts
@@ -25,29 +25,32 @@ export type PostsDTO = {
 
 /** Check CategoryDTO type. */
 export const isCategoryDTO = (obj: any): obj is CategoryDTO => (
-  !!obj.name
-  && typeof obj.name === 'string'
+  obj === Object(obj)
+  && 'name' in obj && typeof obj.name === 'string'
 );
 
 /** Check CategoriesDTO type. */
 export const isCategoriesDTO = (obj: any): obj is CategoriesDTO => (
-  !!obj.results
+  obj === Object(obj)
+  && 'results' in obj && obj.results === Object(obj.results)
   && obj.results.every((element: any) => isCategoryDTO(element))
 );
 
 /** Check PostDTO type. */
 export const isPostDTO = (obj: any): obj is PostDTO => (
-  !!obj.title && typeof obj.title === 'string'
-  && !!obj.imageURL && typeof obj.imageURL === 'string'
-  && !!obj.body && typeof obj.body === 'string'
-  && !!obj.created && typeof obj.created === 'string'
-  && !!obj.category && typeof obj.category === 'string'
+  obj === Object(obj)
+  && 'title' in obj && typeof obj.title === 'string'
+  && 'imageURL' in obj && typeof obj.imageURL === 'string'
+  && 'body' in obj && typeof obj.body === 'string'
+  && 'created' in obj && typeof obj.created === 'string'
+  && 'category' in obj && typeof obj.category === 'string'
 );
 
 /** Check PostsDTO type. */
 export const isPostsDTO = (obj: any): obj is PostsDTO => (
-  !!obj.chunksLeft && typeof obj.chunksLeft === 'number'
-  && !!obj.posts && typeof obj.posts === 'object'
+  obj === Object(obj)
+  && 'chunksLeft' in obj && typeof obj.chunksLeft === 'number'
+  && 'posts' in obj && obj.posts === Object(obj.posts)
   && obj.posts.every((element: any) => isPostDTO(element))
 );
 
