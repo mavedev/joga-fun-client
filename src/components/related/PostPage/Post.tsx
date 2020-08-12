9a0b9a616fd452b2897dd6ddbc3609a1720696e0
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-12 22:54:44 +0300

    Post display style updated

--- src/components/related/PostPage/Post.tsx
+++ src/components/related/PostPage/Post.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import withCSSModule from 'react-css-modules';
 import { Card as BCard } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 import { PostDTO } from 'misc/dal';
 import { PostPagePostStyles as styles } from 'styles';
@@ -13,35 +14,41 @@ const getActualPost = (data: PostDTO) => (
   <>
     <div styleName='Post__TitleAndImageWrapper'>
       <h2 className='card-title'>{data.title}</h2>
-      <img
-        className='card-img-top'
-        styleName='Post__Image'
-        src={data.imageURL}
-        alt=''
-        crossOrigin='anonymous'
-      />
+      <div styleName='Post__ImageWrapper'>
+        <img
+          className='card-img-top'
+          styleName='Post__Image'
+          src={data.imageURL}
+          alt=''
+          crossOrigin='anonymous'
+        />
+      </div>
     </div>
     <p className='card-text'>{`${data.body}`}</p>
   </>
 );
 
 /** Not found message. */
-const getDummy = () => (
+const getDummy = ({ t: translator }: ReturnType<typeof useTranslation>) => (
   <div styleName='Post__TitleAndImageWrapper'>
-    <h2 className='card-title'>:(</h2>
+    <h2 className='card-title'>{translator('NoPostFound')}</h2>
   </div>
 );
 
 /** The post component itself. */
-const Post: React.FC<OwnProps> = ({ data }) => (
-  <BCard styleName='Post__BCard'>
-    <div className='card-body'>
-      {data.id ? getActualPost(data) : getDummy()}
-    </div>
-    <div className='card-footer text-muted'>
-      <p>{data.created}</p>
-    </div>
-  </BCard>
-);
+const Post: React.FC<OwnProps> = ({ data }) => {
+  const translationResponse = useTranslation();
+
+  return (
+    <BCard styleName='Post__BCard'>
+      <div className='card-body'>
+        {data.id ? getActualPost(data) : getDummy(translationResponse)}
+      </div>
+      <div className='card-footer text-muted'>
+        <p>{data.created}</p>
+      </div>
+    </BCard>
+  );
+};
 
 export default withCSSModule(Post, styles);
