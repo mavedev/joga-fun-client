f499e489cb0ee21d14e6d45fdd60de5920b9bafb
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-13 22:05:36 +0300

    Login action on Enter press implemented

--- src/components/related/LoginPage/LoginForm.tsx
+++ src/components/related/LoginPage/LoginForm.tsx
@@ -9,17 +9,27 @@ type OwnProps = {
   doLoginCallback: (username: string, password: string) => void;
 };
 
+/** Contraction for KeyboardEvent type. */
+type KeyEvent = React.KeyboardEvent<HTMLInputElement>;
+
 /** The form containing auth fields. */
 const LoginForm: React.FC<OwnProps> = ({ doLoginCallback }) => {
   const { t: translator } = useTranslation();
   const usernameRef = React.useRef<HTMLInputElement>(null);
   const passwordRef = React.useRef<HTMLInputElement>(null);
+
   const login = React.useCallback(() => {
     doLoginCallback(
       usernameRef.current?.value || '',
       passwordRef.current?.value || ''
     );
   }, [usernameRef, passwordRef, doLoginCallback]);
+
+  const handleKey = React.useCallback((key: KeyEvent) => {
+    if (key.charCode === 13) {
+      login();
+    }
+  }, [login]);
 
   return (
     <form>
@@ -29,6 +39,7 @@ const LoginForm: React.FC<OwnProps> = ({ doLoginCallback }) => {
           type='text'
           className='form-control'
           placeholder={translator('Login')}
+          onKeyPress={handleKey}
         />
       </div>
       <div className='form-group'>
@@ -37,6 +48,7 @@ const LoginForm: React.FC<OwnProps> = ({ doLoginCallback }) => {
           type='password'
           className='form-control'
           placeholder={translator('Password')}
+          onKeyPress={handleKey}
         />
       </div>
       <LoginButton action={login} />
