6bd79daf2aafa3c258f8b9c5c2380b3d0c65fb52
Cirill Usatchoff <usatchoff@gmail.com>
2020-08-13 23:49:13 +0300

    The publish button got callback action

--- src/components/related/NewPostPage/TopLevelBar/PublishButton.tsx
+++ src/components/related/NewPostPage/TopLevelBar/PublishButton.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button as BButton } from 'react-bootstrap';
 
+/** Normal compenent's props. */
+type OwnProps = { action: () => void; };
+
 /** A button which main purpose is to call publish action. */
-const PublishButton: React.FC<{}> = () => {
+const PublishButton: React.FC<OwnProps> = ({ action }) => {
   const { t: translator } = useTranslation();
 
   return (
-    <BButton variant='light'>
+    <BButton variant='light' onClick={() => { action(); }}>
       {translator('Publish')}
     </BButton>
   );
